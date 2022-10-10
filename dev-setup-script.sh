7fe03d5560f97e8561c9647edcf6a849392f93b3
Oliver <20188437+olivergrabinski@users.noreply.github.com>
2022-10-10 20:29:15 +0200

    Change installation order

--- dev-setup-script.sh
+++ dev-setup-script.sh
@@ -1,9 +1,9 @@
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install brew formulae and casks
 brew install $(cat brew-packages.txt)
 brew install --cask $(cat brew-casks.txt)
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
